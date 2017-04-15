2bc79aa934eb54954a6494b38e0f570ab1a5f354
Kaleb Klein <klein.jae@gmail.com>
2017-04-15 01:21:55 -0400

    Initial branch commit
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$success = '#25ae19';
+$danger = '#ff391f';
+
+// $info = [
+//     'color' => $success,
+//     'status' => 'Online',
+// ];
+
+// require __DIR__.'/svg.php';
+
+// the following is used to convert int colors to hex
+// $col = 16726303;
+// $hex = dechex($col);
+
+// while (count(str_split($hex)) < 6) {
+//     $temp = $hex;
+//     $hex = '0'.$temp;
+// }
+
+// echo $hex;
