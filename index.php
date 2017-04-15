a1f457b19f2e83d56581df705d5db70b15e924a8
Kaleb Klein <klein.jae@gmail.com>
2017-04-15 03:30:43 -0400

    Working????
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- index.php
+++ index.php
@@ -19,50 +19,6 @@ if (file_exists(getcwd().'/../../anubisbot/bot_online')) {
 
 require __DIR__.'/svg.php';
 
-// use Curl\Curl;
-// use Ratchet\Client as DC;
-
-// $guildid = env('GUILD_ID', '');
-
-// $curl = new Curl();
-// $curl->setHeader('Authorization', 'Bot '.env('TOKEN', ''));
-// $curl->setHeader('User-Agent', 'AnubisBot Back End');
-// $curl->setHeader('Content-type', 'application/json');
-// $curl->get('https://discordapp.com/api/v6/gateway/bot');
-
-// $wss = $curl->response->url;
-
-// $op = [
-//     'op' => 8,
-//     'd' => null,
-//     's' => 0,
-//     't' => 'GUILD_CREATE',
-// ];
-
-// $ws = DC\connect($wss, $op)->then(function ($c) {
-//     dd($c);
-// });
-
-// dd($ws);
-
-// use RestCord\DiscordClient;
-
-// $discord = new DiscordClient(['token' => env('TOKEN', '')]);
-
-// // dd($discord);
-
-// // $gid = (int)env('GUILD_ID', '');
-// // var_dump($gid);
-// // // dd($gid);
-// dd($discord->guild->getGuildRoles(['guild.id' => $guildid]));
-
-// $info = [
-//     'color' => $success,
-//     'status' => 'Online',
-// ];
-
-// require __DIR__.'/svg.php';
-
 // the following is used to convert int colors to hex
 // $col = 16726303;
 // $hex = dechex($col);
