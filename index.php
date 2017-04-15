3dce9c8bdd9b42404615326695997701686cba35
Kaleb Klein <klein.jae@gmail.com>
2017-04-15 19:32:25 -0400

    Added no-caching on image
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
-// require_once __DIR__.'/vendor/autoload.php';
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header('Content-type: image/svg+xml');
 
 $success = '#25ae19';
 $danger = '#ff391f';
