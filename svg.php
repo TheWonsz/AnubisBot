d2b8b6c9373a1a2f3a7c28cdfeab047c2f9b5099
Kaleb Klein <klein.jae@gmail.com>
2017-04-20 21:14:11 -0400

    updated svg.php to have text shadows
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- svg.php
+++ svg.php
@@ -1,1 +1,12 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="103" height="20"><g shape-rendering="crispEdges"><path fill="#555" d="M0 0h53v20H0z"/><path fill="<?=$info['color']?>" d="M53 0h50v20H53z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="27.5" y="14">Status</text><text x="77" y="14"><?=$info['status']?></text></g></svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="103" height="20">
+    <g shape-rendering="crispEdges">
+        <path fill="#555" d="M0 0h53v20H0z"/>
+        <path fill="<?=$info['color']?>" d="M53 0h50v20H53z" />
+    </g>
+    <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
+        <text x="27.5" y="15" fill="#010101" fill-opacity=".3">Status</text>
+        <text x="27.5" y="14">Status</text>
+        <text x="77" y="15" fill="#010101" fill-opacity=".3"><?=$info['status']?></text>
+        <text x="77" y="14"><?=$info['status']?></text>
+    </g>
+</svg>
