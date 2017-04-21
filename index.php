c79faf78f277230a534c64a7588c4a3eaf563859
Kaleb Klein <klein.jae@gmail.com>
2017-04-20 20:58:07 -0400

    Added readme
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
 header('Content-type: image/svg+xml');
 
 $success = '#25ae19';
---
+++ readme.md
@@ -0,0 +1,6 @@
+# AnubisBot Status Branch
+This branch is used to do a status check on AnubisBot. (Built specifically for me, but can easilly be adapted to any instance)
+
+## Output
+The output is an SVG image as shown here: [![AnubisBot Status](https://api.kalebklein.com/anubisbot_status)](https://api.kalebklein.com/anubisbot_status
+)
