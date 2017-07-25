1a42c3d14c9a56ac94aa506e9ad819f38cfb834e
Kaleb Klein <klein.jae@gmail.com>
2017-07-24 20:16:12 -0400

    Using DiscordBots site to get bot status
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once __DIR__.'/vendor/autoload.php';
+
+use Curl\Curl;
+
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
@@ -7,28 +11,26 @@ header('Content-type: image/svg+xml');
 
 $success = '#25ae19';
 $danger = '#ff391f';
+$baseurl = 'https://discordbots.org/bot/327322469197283328';
 
-if (file_exists(getcwd().'/../../../anubisbot/storage/bot_online')) {
-    $info = [
-        'color'  => $success,
-        'status' => 'online',
-    ];
-} else {
-    $info = [
-        'color'  => $danger,
-        'status' => 'offline',
-    ];
+$curl = new Curl();
+$curl->get($baseurl);
+
+$status = 'OFFLINE';
+
+preg_replace_callback('/(ONLINE|OFFLINE)/i', function ($m) use (&$status) {
+    $status = $m[1];
+}, $curl->response);
+
+$info['status'] = $status;
+
+switch (strtolower($status)) {
+    case 'online':
+        $info['color'] = $success;
+        break;
+    case 'offline':
+        $info['color'] = $danger;
+        break;
 }
 
 require __DIR__.'/svg.php';
-
-// the following is used to convert int colors to hex
-// $col = 16726303;
-// $hex = dechex($col);
-
-// while (count(str_split($hex)) < 6) {
-//     $temp = $hex;
-//     $hex = '0'.$temp;
-// }
-
-// echo $hex;
